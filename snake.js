7eb10f37b81b469681307370fc2ee28867527ba2
Anouar9172 <94382980+Anouar9172@users.noreply.github.com>
2022-09-13 14:11:29 +0200

    1

--- snake.js
+++ snake.js
@@ -3,6 +3,7 @@ let context = canvas.getContext('2d');
 
 let snakeX = 200;
 let snakeY = 200;
+let direction = null;
 
 // Draw the background
 function drawBackrgound(){
@@ -15,12 +16,24 @@ function drawSnake(){
   context.fillStyle = 'white';
   context.fillRect(snakeX, snakeY, 20, 20);
 }
-
 function update() {
-  snakeX += 20;
-  drawBackrgound();
-  drawSnake();
+  if(direction == 'right') {
+    snakeX += 20;
+  } else if(direction == 'left') {
+    snakeX -= 20;
+  } else if(direction == 'up'){
+  snakeY -= 20;
+} else if(direction == 'down'){
+  snakeY += 20;
 }
+drawBackrgound();
+drawSnake();
+}
+
+function changeDirection(event) {
+  console.log('direction');
+}
+
 drawBackrgound();
 drawSnake();
 setInterval(update,1000);
