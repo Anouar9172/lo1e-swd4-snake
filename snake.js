646b15d231bf13fd7ef82d51b1e1a284cbe1cc15
Anouar9172 <94382980+Anouar9172@users.noreply.github.com>
2022-10-04 13:05:51 +0200

    1

--- snake.js
+++ snake.js
@@ -3,6 +3,19 @@ let ctx = canvas.getContext("2d");
 
 let snakeX = 200;
 let snakeY = 200;
+let snake = [
+  {
+  x: 200,
+  y: 200
+},
+{
+  x:220,
+  y:200
+}
+];
+
+let foodX;
+let foodY;
 
 let direction = null;
 
@@ -12,32 +25,45 @@ function drawBackground() {
 }
 
 function drawSnake() {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(snakeX, snakeY, 20, 20);
+    ctx.fillStyle = "white";
+
+    for (let index = 0; index < snake.length; index++) {
+      ctx.fillRect(snake[index].x , snake[index].y , 20, 20);
+    }
 }
 
 function update() {
+  let tailIndex = snake.length -1;
+  let tailX = snake[tailIndex].x;
+  let tailY = snake[tailIndex].y;
+
+
+  for (let index = snake.length - 1; index > 0; index--){
+    snake[index].x = snake[index - 1].x;
+    snake[index].y = snake[index - 1].y;
+  }
+
     if(direction == 'right') {
-      if (snakeX < 380){
-        snakeX +=  20;
+      if (snake[0].x < 380){
+        snake[0].x +=  20;
       } else {
         gameOver();
       }
     } else if(direction == 'left') {
-        if(snakeX > 0) {
-            snakeX -= 20;
+        if(snake[0].x > 0) {
+            snake[0].x -= 20;
         } else {
             gameOver();
         }
     } else if(direction == 'up') {
-        if(snakeY > 0) {
-        snakeY -= 20;
+        if(snake[0].y > 0) {
+        snake[0].y -= 20;
       } else {
         gameOver();
       }
     } else if(direction == 'down') {
-      if (snakeY < 380){
-        snakeY += 20;
+      if (snake[0].y < 380){
+        snake[0].y += 20;
       } else {
         gameOver();
       }
@@ -49,6 +75,16 @@ function update() {
     drawFood();
 }
 
+
+if (foodX == snake[0].x && foodY == snake[0].y) {
+  alert('hap!');
+
+  snake.push({
+    x:tailX,
+    y:tailY
+  })
+  spawnFood();
+}
 
 function changeDirection(event) {
 }
@@ -64,8 +100,8 @@ function spawnFood () {
 }
 
 function drawFood (){
-  context.fillStyle = "red";
-  conext.fillrect(foodX, foodY, 20, 20);
+  ctx.fillStyle = "red";
+  ctx.fillRect(foodX, foodY, 20, 20);
 }
 
 
@@ -80,6 +116,8 @@ function changeDirection(event) {
         direction = 'left';
     }
 }
+
+
 drawBackground();
 drawSnake();
 setInterval(update, 300);
